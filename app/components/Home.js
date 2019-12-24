6f48869898c2c5d9bdcd95dabb5cfeb95dffeea8
inovramadani <iqbal.slf@gmail.com>
2019-12-24 20:02:50 +0700

    feat: initial homepage

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,7 +1,5 @@
 // @flow
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import routes from '../constants/routes';
 import styles from './Home.css';
 
 type Props = {};
@@ -9,45 +7,16 @@ type Props = {};
 export default class Home extends Component<Props> {
   props: Props;
 
-  state = {
-    posts : []
-  }
-
   render() {
-    const { posts } = this.state
-
     return (
       <div className={styles.container} data-tid="container">
-        <h2>Home</h2>
-        <Link to={routes.COUNTER}>to Counter</Link>
-        <button onClick={this.handleClickShow}>Show</button>
-        {
-          posts &&
-          posts.map(post => (
-            <div>
-              <span>{post.title}</span>
-            </div>
-          ))
-        }
+        <div className={styles.headline}>
+          <h2>Timy</h2>
+          <p>Time tracker</p>
+          <p>for</p>
+          <p>remote working</p>
+        </div>
       </div>
     );
-  }
-
-  handleClickShow = () => {
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-      method: 'POST',
-      body: JSON.stringify({
-        title: 'iqbal',
-        body: 'novramadani',
-        userId: 1
-      }),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8"
-      }
-    })
-    .then(response => response.json())
-      .then(post => {
-        this.setState({ posts: [post] })
-      })
   }
 }
